f75a71ef02ae6060fe9a457295f78765baa8efcf
John Fiorilla <john.fiorilla@loan-street.com>
2018-03-29 15:25:06 -0400

    gen idmap

--- index.js
+++ index.js
@@ -46,6 +46,21 @@ const collectIds = childs => {
   return ids;
 };
 
+const generateIdMap = ids => {
+  const idMap = {};
+  ids.forEach(id => {
+    idMap[id] = uuid();
+  });
+  return idMap;
+};
+
+// const replaceIds = (childs, uniqueIds) => {
+//   childs.forEach(child => {
+//       if (child.mask.includes('#')) child.mask = `url#()`
+//   })
+//   return childs;
+// };
+
 const editFile = (file = readFile()) => {
   console.log(file);
   svgson(file, {}, result => {
@@ -54,8 +69,14 @@ const editFile = (file = readFile()) => {
     console.log('\n');
     console.log(childs[0].childs[0]);
     const ids = collectIds(childs);
+    const idMap = generateIdMap(ids);
     console.log('\n');
-    console.log(ids);
+    console.log(idMap);
+    // const uniqueIds = ids.map(id => ({ id, uniqueId: uuid() }));
+    // console.log(uniqueIds);
+    // const newJson = replaceIds(childs, uniqueIds);
+    // console.log('\n');
+    // console.log(newJson);
   });
 };
 
